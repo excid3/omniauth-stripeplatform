839bd2af71989266c94f1ed0f9139871e17c4033
Anthony Joseph <afresta@gmail.com>
2012-06-01 13:19:20 -0500

    test

--- lib/omniauth/strategies/stripeplatform.rb
+++ lib/omniauth/strategies/stripeplatform.rb
@@ -2,6 +2,7 @@ require 'omniauth/strategies/oauth2'
 require 'base64'
 require 'openssl'
 require 'rack/utils'
+require 'multi_json'
 
 module OmniAuth
   module Strategies
@@ -20,47 +21,19 @@ module OmniAuth
       end
 
       def callback_phase
+	code = params[:code]
+
+    # Attach the redirect_uri and our live-mode API key
+     params = { 
+           	:redirect_uri => redirect_url,
+                 :headers => {'Authorization' => "Bearer #{api_key}"}
+                     }   
         return fail!(:invalid_credentials) unless identity
         super
       end
 
-      def other_phase
-        if on_registration_path?
-          if request.get?
-            registration_form
-          elsif request.post?
-            registration_phase
-          end
-        else
-          call_app!
-        end
-      end
 
-      def registration_form
-        OmniAuth::Form.build(:title => 'Register Identity') do |f|
-          options[:fields].each do |field|
-            f.text_field field.to_s.capitalize, field.to_s
-          end
-          f.password_field 'Password', 'password'
-          f.password_field 'Confirm Password', 'password_confirmation'
-        end.to_response
-      end
 
-      def registration_phase
-        attributes = (options[:fields] + [:password, :password_confirmation]).inject({}){|h,k| h[k] = request[k.to_s]; h}
-        @identity = model.create(attributes)
-        if @identity.persisted?
-          env['PATH_INFO'] = callback_path
-          callback_phase
-        else
-          if options[:on_failed_registration]
-            self.env['omniauth.identity'] = @identity
-            options[:on_failed_registration].call(self.env)
-          else
-            registration_form
-          end
-        end
-      end
 
       uid{ identity.uid }
       info{ identity.info }
