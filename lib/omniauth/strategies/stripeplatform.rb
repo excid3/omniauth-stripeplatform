1697a148085d922ce5cde4f14908e1c20bac79cf
Anthony F <afresta@gmail.com>
2012-06-01 22:34:03 -0500

    test

--- lib/omniauth/strategies/stripeplatform.rb
+++ lib/omniauth/strategies/stripeplatform.rb
@@ -18,24 +18,43 @@ module OmniAuth
         :token_url => '/oauth2/token'
 		             }
 	
-      option :access_token_options, {
-        :header_format => 'OAuth %s',
-        :param_name => 'access_token'
-      }
-     
-
-
-	 info do
-        {
-        'email' => email,
-         }
-           end
-
-  def build_access_token
-    super.tap do |token|
-    token.options.merge!(access_token_options)
-       end
-   end
+uid { raw_info['id'] }
+      
+      info do
+        prune!({
+          'nickname' => raw_info['username'],
+          'name' => raw_info['full_name'],
+          'image' => raw_info['avatar_url'],
+          'description' => raw_info['description'],
+          'urls' => {
+            'Website' => raw_info['website']
+          },
+          'location' => raw_info['city']
+        })
+      end
+      
+      credentials do
+        prune!({
+          'expires' => access_token.expires?,
+          'expires_at' => access_token.expires_at
+        })
+      end
+      
+      extra do
+        prune!({
+          'raw_info' => raw_info
+        })
+      end
+      
+      def raw_info
+        @raw_info ||= access_token.get('/me.json').parsed
+      end
+      
+      def build_access_token
+        super.tap do |token|
+          token.options.merge!(access_token_options)
+        end
+      end
 
       def access_token_options
         options.access_token_options.inject({}) { |h,(k,v)| h[k.to_sym] = v; h }
@@ -48,6 +67,14 @@ module OmniAuth
         end
       end
       
+      private
+      
+      def prune!(hash)
+        hash.delete_if do |_, value| 
+          prune!(value) if value.is_a?(Hash)
+          value.nil? || (value.respond_to?(:empty?) && value.empty?)
+        end
+      end      
 
 
     end
