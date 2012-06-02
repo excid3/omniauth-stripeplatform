919e2e967e613e6307aefd3a218486a0e815ed59
Anthony F <afresta@gmail.com>
2012-06-02 13:16:28 -0500

    test

--- lib/omniauth/strategies/stripeplatform.rb
+++ lib/omniauth/strategies/stripeplatform.rb
@@ -10,66 +10,10 @@ module OmniAuth
 	:token_url => '/oauth/token'
 		             }
 	option :token_params, {
-	:parse => :query
+	:parse => :json
 	}
 
-	option :access_token_options, {
-        :header_format => "",
-        :param_name => 'access_token'
-      }
-
-
-uid { raw_info['id'] }
       
-      info do
-        prune!({
-          'email' => raw_info['email']
-        })
-      end
-      
-      credentials do
-        prune!({
-          'expires' => access_token.expires?,
-          'expires_at' => access_token.expires_at
-        })
-      end
-      
-      extra do
-        prune!({
-          'raw_info' => raw_info
-        })
-      end
-      
-      def raw_info
-        @raw_info ||= access_token.get('').parsed
-      end
-      
-      def build_access_token
-        super.tap do |token|
-          token.options.merge!(access_token_options)
-        end
-      end
-
-      def access_token_options
-        options.access_token_options.inject({}) { |h,(k,v)| h[k.to_sym] = v; h }
-      end
-
-      def authorize_params
-        super.tap do |params|
-          # params.merge!(:display => request.params['display']) if request.params['display']
-          params[:scope] ||= DEFAULT_SCOPE
-        end
-      end
-      
-      private
-      
-      def prune!(hash)
-        hash.delete_if do |_, value| 
-          prune!(value) if value.is_a?(Hash)
-          value.nil? || (value.respond_to?(:empty?) && value.empty?)
-        end
-      end      
-
 
     end
   end
