4942c43f68242c997ab9b4d8587096bc9b4f8062
Anthony F <afresta@gmail.com>
2012-06-01 21:36:25 -0500

    test

--- lib/omniauth/strategies/stripeplatform.rb
+++ lib/omniauth/strategies/stripeplatform.rb
@@ -13,7 +13,7 @@ module OmniAuth
       option :client_options, {
         :site => 'https://manage.stripe.com',
         :authorize_url => '/oauth2/authorize',
-        :token_url => '/oauth2/token'
+        :token_url => '/oauth2/token',
 	      :header_format => 'OAuth %s',
 	      :headers => {'Authorization' => "Bearer yebV2wGX65K8tSfsFJZgCsuIi3Y0pIq2"},
               :param_name => 'access_token'
