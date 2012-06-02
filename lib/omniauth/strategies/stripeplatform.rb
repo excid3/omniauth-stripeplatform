b4ff0fd5a7d5e612b50d56387d0dc1e4ba45b40f
Anthony F <afresta@gmail.com>
2012-06-01 22:44:48 -0500

    test

--- lib/omniauth/strategies/stripeplatform.rb
+++ lib/omniauth/strategies/stripeplatform.rb
@@ -21,8 +21,7 @@ module OmniAuth
 	option :access_token_options, {
         :header_format => 'OAuth %s',
         :param_name => 'access_token'
-	:response_type => 'code',
-        :mode => :header
+	:response_type => 'code'
       }
 
 
