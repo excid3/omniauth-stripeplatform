f263dfc2b0127e5700458f411ba4cf0da7fe2165
Anthony F <afresta@gmail.com>
2012-06-02 13:50:49 -0500

    test

--- lib/omniauth/strategies/stripeplatform.rb
+++ lib/omniauth/strategies/stripeplatform.rb
@@ -21,6 +21,63 @@ module OmniAuth
 
 	}
 
+	option :access_token_options, {
+        :header_format => 'OAuth %s',
+        :param_name => 'access_token'
+      }
+
+
+uid { raw_info['id'] }
+      
+      info do
+        prune!({
+                   
+          'email' => raw_info['email']
+        })
+      end
+      
+      credentials do
+        prune!({
+          'expires' => access_token.expires?,
+          'expires_at' => access_token.expires_at
+        })
+      end
+      
+      extra do
+        prune!({
+          'raw_info' => raw_info
+        })
+      end
+      
+      def raw_info
+        @raw_info ||= access_token.get('/me.json').parsed
+      end
+      
+      def build_access_token
+        super.tap do |token|
+          token.options.merge!(access_token_options)
+        end
+      end
+
+      def access_token_options
+        options.access_token_options.inject({}) { |h,(k,v)| h[k.to_sym] = v; h }
+      end
+
+      def authorize_params
+        super.tap do |params|
+          # params.merge!(:display => request.params['display']) if request.params['display']
+          params[:scope] ||= DEFAULT_SCOPE
+        end
+      end
+      
+      private
+      
+      def prune!(hash)
+        hash.delete_if do |_, value| 
+          prune!(value) if value.is_a?(Hash)
+          value.nil? || (value.respond_to?(:empty?) && value.empty?)
+        end
+
       
 
     end
