a4ac2786dba6c543aa3973fe919ab498d0cb268a
Anthony F <afresta@gmail.com>
2012-06-03 18:26:12 -0500

    test

--- lib/omniauth/strategies/stripeplatform.rb
+++ lib/omniauth/strategies/stripeplatform.rb
@@ -15,24 +15,26 @@ module OmniAuth
 	:authorize_url => '/oauth/authorize',
 	:token_url => '/oauth/token'
 		             }
+
 		option :token_params, {
 		  :parse => :query
 			}
-	option :access_token_options, {
-  :header_format => "OAuth %s",
-  :param_name => 'access_token'
-}
 
-option :authorize_params, {
-  :header_format => "Bearer %s(:client_secret)"
-}
+
+	option :access_token_options, {
+	   :header_format => "OAuth %s",
+	   :param_name => 'access_token'
+			}
+
+	option :authorize_params, {
+	    :header_format => "Bearer %s(:client_secret)"
+		}
   
 
-      def request_phase
-        authorize_params[:headers] = "Authorize: Bearer #{client.secret}"
+	def request_phase
+        authorize_params[:headers][:authorize] = "Bearer #{client.secret}"
         redirect client.auth_code.authorize_url({:redirect_uri => callback_url}.merge(authorize_params))
       end
-
 
 
        uid{ raw_info['id'] }
