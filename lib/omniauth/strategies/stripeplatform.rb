816bde7d99752069c5ede53e8cc011ebb06ee5c5
Anthony F <afresta@gmail.com>
2012-06-02 19:16:39 -0500

    test

--- lib/omniauth/strategies/stripeplatform.rb
+++ lib/omniauth/strategies/stripeplatform.rb
@@ -8,8 +8,6 @@ module OmniAuth
       DEFAULT_SCOPE = 'read_write'
       DEFAULT_RESPONSE_TYPE = 'code'
       DEFAULT_GRANT = 'authorization_code'
-      CLIENT_SECRET = 'yebV2wGX65K8tSfsFJZgCsuIi3Y0pIq2'
-
 
       option :name, "stripeplatform"
 
@@ -20,22 +18,11 @@ module OmniAuth
 	:token_url => '/oauth/token'
 		             }
 
+	def request_phase
+        super
+       end	
+
 	
- def authorize_params
-        super.tap do |params|
-          params[:response_type] ||= DEFAULT_RESPONSE_TYPE
-          params[:client_id] = client.id
-        end
-      end
-
-      def token_params
-        super.tap do |params|
-          params[:grant_type] ||= DEFAULT_GRANT
-          params[:client_id] = client.id
-          params[:client_secret] = CLIENT_SECRET
-        end
-      end
-
 
     end
   end
