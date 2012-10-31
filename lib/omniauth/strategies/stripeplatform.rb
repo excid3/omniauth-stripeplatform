4a7323cfe6eadeea43575541acf1e3ebecb088f2
Chris Oliver <excid3@gmail.com>
2012-10-31 13:12:55 -0500

    Clean up formatting

--- lib/omniauth/strategies/stripeplatform.rb
+++ lib/omniauth/strategies/stripeplatform.rb
@@ -1,70 +1,53 @@
 require 'omniauth-oauth2'
 require 'stripe'
 
-
 module OmniAuth
   module Strategies
     class StripePlatform < OmniAuth::Strategies::OAuth2
-
-     DEFAULT_SCOPE = 'read_write'
+      DEFAULT_SCOPE = 'read_write'
 
       option :name, "stripeplatform"
 
       option :client_options, {
         :site => 'https://manage.stripe.com',
-	:authorize_url => '/oauth/authorize',
-	:token_url => '/oauth/token'
-		             }
+        :authorize_url => '/oauth/authorize',
+        :token_url => '/oauth/token'
+      }
 
-		option :token_params, {
-		  :parse => :query
-			}
+      option :token_params, {
+        :parse => :query
+      }
 
+      option :access_token_options, {
+        :header_format => "OAuth %s",
+        :param_name => 'access_token'
+      }
 
-	option :access_token_options, {
-	   :header_format => "OAuth %s",
-	   :param_name => 'access_token'
-			}
+      option :authorize_params, {
+        :header_format => "Bearer %s(:client_secret)"
+      }
 
-	option :authorize_params, {
-	    :header_format => "Bearer %s(:client_secret)"
-		}
-  
-
-	def request_phase
+      def request_phase
         authorize_params[:headers] = {:authorize => "Bearer #{client.secret}"}
         redirect client.auth_code.authorize_url({:redirect_uri => callback_url}.merge(authorize_params))
       end
 
-       uid{ raw_info['id'] }
+      uid{ raw_info['id'] }
 
-
-	info do
-	{
-	:id => raw_info['id']        
-
-        }
+      info do
+        { :id => raw_info['id'] }
       end
 
       extra do
-        {
-          'raw_info' => raw_info
-        }
+        { 'raw_info' => raw_info }
       end
 
-
-
-
-	def raw_info
-	end
-
-	
+      def raw_info
+      end
 
     end
   end
 end
 
-
 OmniAuth.config.add_camelization 'stripeplatform', 'StripePlatform'
-
 
